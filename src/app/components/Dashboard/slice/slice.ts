824504a5d154508556753a635ee6ed643012a92d
Derek Lavigne <derek.lavigne@comcast.net>
2022-09-02 17:33:56 -0400

    Small reducer refactor

--- src/app/components/Dashboard/slice/slice.ts
+++ src/app/components/Dashboard/slice/slice.ts
@@ -2,42 +2,6 @@ import { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from 'utils/@reduxjs/toolkit';
 import { DashboardState, Matchup, SeasonMap, Team, TeamMap, TeamRecord } from './types';
 
-// Reducers
-function setSelectedSeason(state, action: PayloadAction<number>) {
-  state.selectedSeason = action.payload;
-}
-
-function setSelectedWeek(state, action: PayloadAction<number>) {
-  state.selectedWeek = action.payload;
-}
-
-function setLoadingRecords(state, action: PayloadAction<boolean>) {
-  state.loadingRecords = action.payload;
-}
-
-function setLoadingMatchups(state, action: PayloadAction<boolean>) {
-  state.loadingMatchups = action.payload;
-}
-
-function setLoadingSeasons(state, action: PayloadAction<boolean>) {
-  state.loadingSeasons = action.payload;
-}
-
-function setSeasons(state, action: PayloadAction<SeasonMap>) {
-  state.seasons = action.payload;
-}
-
-function setTeams(state, action: PayloadAction<Team[]>) {
-  action.payload.forEach(t => (state.teams[t.id] = t));
-}
-
-function setRecords(state, action: PayloadAction<TeamRecord[]>) {
-  state.records = action.payload;
-}
-
-function setMatchups(state, action: PayloadAction<Matchup[]>) {
-  state.matchups = action.payload;
-}
 
 export const initialState: DashboardState = {
   selectedSeason: 0,
@@ -55,15 +19,33 @@ const slice = createSlice({
   name: 'dashboard',
   initialState,
   reducers: {
-    setSelectedSeason,
-    setSelectedWeek,
-    setLoadingRecords,
-    setLoadingMatchups,
-    setLoadingSeasons,
-    setSeasons,
-    setTeams,
-    setRecords,
-    setMatchups,
+    setSelectedSeason(state, action: PayloadAction<number>) {
+      state.selectedSeason = action.payload;
+    },
+    setSelectedWeek(state, action: PayloadAction<number>) {
+      state.selectedWeek = action.payload;
+    },
+    setLoadingRecords(state, action: PayloadAction<boolean>) {
+      state.loadingRecords = action.payload;
+    },
+    setLoadingMatchups(state, action: PayloadAction<boolean>) {
+      state.loadingMatchups = action.payload;
+    },
+    setLoadingSeasons(state, action: PayloadAction<boolean>) {
+      state.loadingSeasons = action.payload;
+    },
+    setSeasons(state, action: PayloadAction<SeasonMap>) {
+      state.seasons = action.payload;
+    },
+    setTeams(state, action: PayloadAction<Team[]>) {
+      action.payload.forEach(t => (state.teams[t.id] = t));
+    },
+    setRecords(state, action: PayloadAction<TeamRecord[]>) {
+      state.records = action.payload;
+    },
+    setMatchups(state, action: PayloadAction<Matchup[]>) {
+      state.matchups = action.payload;
+    },
   },
 });
 
